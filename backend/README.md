98925be7d867cde64d40c512af6ee0acc343828d
kolibreath <734780178@qq.com>
2018-12-02 16:53:30 +0800

    [ADD] 后端文档

--- backend/README.md
+++ backend/README.md
@@ -1,5 +1,7 @@
 # 给后端同学写的可能的文档
 
+> 大小写什么的可能需要注意一下 不知道flurl.http 是怎么解析json的
+
 # 登录
 url| header | methods  
 ---|--------|-------- 
@@ -13,10 +15,19 @@ no params
 ## POST DATA:  
 ```
 {
-    email:"734780178@qq.com" //邮件地址 格式：String
-    password:""              //密码 格式:String
+    Email:"734780178@qq.com" //邮件地址 格式：String
+    Password:""              //密码 格式:String
 }
 
+```
+
+## RETURN DATA:  
+
+```
+{
+    Msg:"登陆成功 / 并没有登录成功",
+    Code:200(200 == 成功/ 400 ==  失败)
+}
 ```
 
 # 注册
@@ -31,16 +42,92 @@ no params
 ## POST DATA:  
 ```
 {
-    msg:"7注册成功" // 返回值 
+    Email:"734780178@qq.com" //邮件地址 格式：String
+    Password:""              //密码 格式:String
 }
 
 ```
 
 
-# 发送"邮件"
+## RETURN DATA:  
+
+```
+{
+    Msg:"注册成功/并没有注册陈工"  
+    Code:200/400 //int
+}
+
+```
+
+# 获取前三名玩家！
+
 
 url| header | methods  
 ---|--------|-------- 
-/send/ | Content-Type:application/json |  POST
+/topthree/ | Content-Type:application/json |  GET
+
+## URL Params  
+```
+no params
+```
+## RETURN DATA:  
+
+到时候现在数据库里面初始化三个玩家好了！
+```
+[
+    {
+        Email:"习近平",
+        NickName:"维尼",
+        Score:100
+    },
+    {
+        Email:"江泽民",
+        NickName:"蛤",
+        Score:99
+    }
+    
+]
+```
 
 
+# 挑战某人
+
+> 注意 这是一个GET 请求！
+
+url| header | methods  
+---|--------|-------- 
+/challenge/ | Content-Type:application/json |  GET
+
+## URL Params  
+```
+?email=xxxx 
+```
+## RETURN DATA:  
+
+```
+{
+    Msg:"注册成功/并没有注册陈工"  
+    Code:200/400 //int
+}
+
+```
+
+# 是否收到别人的挑战？
+
+
+url| header | methods  
+---|--------|-------- 
+/status/ | Content-Type:application/json |  GET
+
+## URL Params  
+```
+no params
+```
+## RETURN DATA:  
+
+```
+{
+    Msg:"收到挑战 / 并没有收到挑战",
+    Code:200(200 == 收到挑战 / 404 ==  没有挑战)
+}
+```
--- ui/MineSweeper/MineSweeper/Presenter/IService.cs
+++ ui/MineSweeper/MineSweeper/Presenter/IService.cs
@@ -1,13 +1,98 @@
-﻿using System;
+﻿using Flurl.Http;
+using Flurl;
+using MineSweeper.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace MineSweeper.Presenter
 {
     class  NetService
     {
-        bool async Login(User user);
+        string api = "";
+        /// <summary>
+        /// 登录成功之后返回值
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        async Task<bool> Login(User user)
+        {
+            var requestApi =  api + "login/";
+            //实现一个post 请求并且获取返回值
+            Message message = await requestApi.PostJsonAsync(user).ReceiveJson<Message>();
+            if (message.Code == 200)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// 如果登录成功且返回值为200 return true 
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        async Task<bool> Signin (User user)
+        {
+            var requestApi = api + "signin/";  
+            Message message = await requestApi.PostJsonAsync(user).ReceiveJson<Message>();
+            if (message.Code == 200)
+                return true;
+            else
+                return false;
+        }
+        /// <summary>
+        /// 获取前三位排名的玩家
+        /// </summary>
+        /// <returns></returns>
+        async Task< List< Player>> GetTopPlayers()
+        {
+            
+            var requestApi = api + "topthree/";
+            IList<dynamic> list = await requestApi.GetJsonListAsync();
+            List<Player> players = new List<Player>();
+            foreach( Player player in list)
+            {
+                players.Add(player);
+            }
+
+            return players;
+        }
+
+        /// <summary>
+        /// 挑战某一个玩家
+        /// </summary>
+        /// <param name="player"></param>
+        /// <returns></returns>
+        async Task<bool> Challenge(Player player)
+        {
+            var requestApi = api+"challenge/";
+            Message message = await requestApi
+                .SetQueryParam("email", new { player.Email })
+                .GetJsonAsync<Message>();
+            if (message.Code == 200)
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// 用户获取更新 看自己有没有收到别人的挑战
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns></returns>
+        async Task<bool> GetUpdate(User user)
+        {
+            var requestApi =  api + "status/";
+            Message message = await requestApi.PostJsonAsync(user).ReceiveJson<Message>();
+            if (message.Code == 200)
+                return true;
+            else
+                return false;
+        }
+ 
+      
     }
 }
